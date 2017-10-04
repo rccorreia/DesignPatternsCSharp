131141b720b33b2a985be303576538515ea23926
Ricardo Correia <rdcc89@gmail.com>
2017-10-03 21:25:18 -0300

    Padrão de Projeto Adapter

---
+++ PadrõesDeProjeto/PadrõesEstruturais/Adapter/Carne.cs
@@ -0,0 +1,24 @@
+﻿using PadrõesDeProjeto.PadrõesEstruturais.Adapter.LegacyAPI;
+using System;
+
+namespace PadrõesDeProjeto.PadrõesEstruturais.Adapter
+{
+    public class Carne : CarneModelo
+    {
+        private MeatDatabase _meatDatabase;
+
+        public Carne(string nome) 
+            : base(nome) { }
+
+        public override void CarregarInformaçõesDaCarne()
+        {
+            _meatDatabase = new MeatDatabase();
+            TemperaturaDeCozimentoEmCelsius = _meatDatabase.GetSafeCookTemp(NomeNaLegacyAPI,
+            TemperatureType.Celsius);
+            TemperaturaDeCozimentoEmFahrenheit = _meatDatabase.GetSafeCookTemp(NomeNaLegacyAPI,
+            TemperatureType.Fahrenheit);
+            CaloriasPorGrama = _meatDatabase.GetCaloriesPerOunce(NomeNaLegacyAPI);
+            ProteinaPorGrama = _meatDatabase.GetProteinPerOunce(NomeNaLegacyAPI);
+        }
+    }
+}
---
+++ PadrõesDeProjeto/PadrõesEstruturais/Adapter/LegacyAPI/MeatDatabase.cs
@@ -0,0 +1,63 @@
+﻿namespace PadrõesDeProjeto.PadrõesEstruturais.Adapter.LegacyAPI
+{
+    public class MeatDatabase
+    {
+        public float GetSafeCookTemp(string meat, TemperatureType tempType)
+        {
+            if (tempType == TemperatureType.Fahrenheit)
+            {
+                switch (meat)
+                {
+                    case "beef":
+                    case "pork":
+                        return 145f;
+                    case "chicken":
+                    case "turkey":
+                        return 165f;
+                    default:
+                        return 165f;
+                }
+            }
+            else
+            {
+                switch (meat)
+                {
+                    case "beef":
+                    case "veal":
+                    case "pork":
+                        return 63f;
+                    case "chicken":
+                    case "turkey":
+                        return 74f;
+                    default:
+                        return 74f;
+                }
+            }
+        }
+
+        public float GetCaloriesPerOunce(string meat)
+        {
+            switch (meat.ToLower())
+            {
+                case "beef": return 71;
+                case "pork": return 69;
+                case "chicken": return 66;
+                case "turkey": return 38;
+                default:
+                    return 0;
+            }
+        }
+
+        public float GetProteinPerOunce(string meat)
+        {
+            switch (meat.ToLower())
+            {
+                case "beef": return 7.33f;
+                case "pork": return 7.67f;
+                case "chicken": return 8.57f;
+                case "turkey": return 8.5f;
+                default: return 0f;
+            }
+        }
+    }
+}
